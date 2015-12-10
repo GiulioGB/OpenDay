418b2e84fd5f3bbd3f861264c86353a7869788b4
Ballabio Giulio <ballabio_giulio@INFO1-24.jeanmonnet.local>
2015-12-10 13:55:27 +0100

    Aggiunta cartella con progetto+

---
+++ PROGETTO/WindowsFormsApplication1/WindowsFormsApplication1/Domanda.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class domanda
+    {
+        public String Autore, DataIns, Categoria, Testo, Tipo, Scelte , rispostaData;
+
+        public domanda()
+        {
+            
+        }
+        //public domanda(String Tipo, String Categoria, String Testo, String Scelte, String Autore, String DataIns)
+        //{
+        //    this.Autore = Autore;
+        //    this.DataIns = DataIns;
+        //    this.Categoria = Categoria;
+        //    this.Testo = Testo;
+        //    this.Tipo = Tipo;
+        //    this.Scelte = Scelte;
+        //    this.rispostaData = "";
+        //}
+
+        public void creaDomanda(String[] campi)
+        {
+            this.Tipo = campi[0];
+            this.Categoria = campi[1];
+            this.Testo = campi[2];
+            this.Scelte = campi[3];
+            this.Autore = campi[4];
+            this.DataIns = campi[5];
+            this.rispostaData = "";
+
+        }
+
+        
+        
+        public void rispondi(String rispostaData)
+        {
+            this.rispostaData = rispostaData;
+        }
+
+
+        public String ricercaCorretta()
+        { 
+            String s = "";
+            for(int i=0;i<Scelte.Length;i++)
+            {
+                if (Tipo.Equals("matematica"))
+                {
+                    if (Scelte[0] == 'V')
+                        //da modificare
+                        s = Scelte;
+                        //------------.
+                }
+                else
+                {
+                    if (Scelte[0] == '1')
+                        //da modificare
+                        s = Scelte;
+                       //-------------
+                }
+            
+               
+            }
+            
+            return s;
+        }
+
+        
+        public int correzione(int punteggio)
+        {
+            
+            if(rispostaData.Equals(ricercaCorretta()))
+            {
+                punteggio += 1;
+            }
+            else
+            {
+                //non assegno punti
+            }
+            return punteggio;
+        }
+
+        public String getTipo()
+        {
+            return Tipo;
+        }
+
+
+        //public void visualizzaDomanda()
+        //{
+        //    System.Console.WriteLine(Autore + DataIns + Categoria + Testo +  Tipo + Scelte + rispostaData);
+        
+        
+        //}
+    }
+}
---
+++ PROGETTO/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        gestioneFile Liste;
+        public Form1()
+        {
+            Liste = new gestioneFile();
+            InitializeComponent();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (Liste.getTipo(1).Equals("Risposta aperta"))
+            {
+                label3.Visible = false; //IIS
+                label6.Visible = false; //GENERALITA
+                label5.Visible = false; //CONOS
+                label4.Visible = false; //BENVEN
+                label7.Visible = false; //FINE
+                label10.Visible = true; //TESTO DOMANDA sempre true
+                label11.Visible = false; //DOPO AVER...
+                label12.Visible = false; //PUNTEGGIO CONSI
+                label13.Visible = false; //PUNTEGGIO OTT
+                textBox1.Visible = true; //BARRA BIANCA
+                button8.Visible = false; //INZIO
+                button9.Visible = false; //TEST CON
+                button10.Visible = false; //TEST GENER
+                pictureBox1.Visible = true; //LOGO
+                button2.Visible = false; //CONSEGNA
+                button1.Visible = true; //-->
+                button7.Visible = true; //<--
+                checkBox10.Visible = false;  //sempre cosi
+                checkBox6.Visible = false; 
+                checkBox7.Visible = false;
+                checkBox8.Visible = false;
+                checkBox9.Visible = false;
+
+
+
+
+            }
+            else if (Liste.getTipo(1).Equals("Ripsosta multipla"))
+            {
+
+            }
+            else if (Liste.getTipo(1).Equals("Ripsosta multipla singola"))
+            {
+
+            }
+            else if (Liste.getTipo(1).Equals("Vero o Falso"))
+            {
+
+            }
+                            
+
+
+        }
+    }
+}
