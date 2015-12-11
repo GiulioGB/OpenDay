7b85471cd82add752a77f0a6e1942c479e53d97e
fra <castelli_francesco@INFO1-23.jeanmonnet.local>
2015-12-11 10:01:05 +0100

    Aggiornata classe risposte e file

--- CodiceCastelli/GestioneFile.cs
+++ CodiceCastelli/GestioneFile.cs
@@ -3,10 +3,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace ConsoleApplication1
+namespace WindowsFormsApplication1
 {
-    class GestioneFile
+    class gestioneFile
     {
+        List<domanda> questionario;
+        List<risposte> risposte;
+
+        public gestioneFile()
+        {
+            questionario = new List<domanda>();
+            risposte = new List<risposte>();
+            caricaQ();
+            caricaFileRisposte();
+        }
+
+        //creazione questionario totale!
+        public void caricaQ()
+        {
+            string[] d;
+            string[] allLines = File.ReadAllLines(@"D:\BALLA_GIT\OpenDay\PROGETTO\WindowsFormsApplication1\WindowsFormsApplication1\Domande_Informatica.csv");
+            
+            
+            for (int i = 0; i < 42; i++)
+            {
+                d = allLines[i].Split(';');
+                domanda domand = new domanda();
+                domand.creaDomanda(d);
+                questionario.Add(domand);
+                
+            }
+            //for (int i = 0; i < 42; i++)
+            //{
+            //    questionario.ElementAt(i).visualizzaDomanda();
+            //}
+            
+        }
+        //----------------------------
+
+        public void caricaFileRisposte()
+        {
+            string[] d;
+            string[] allLines = File.ReadAllLines(@"D:\BALLA_GIT\OpenDay\PROGETTO\WindowsFormsApplication1\WindowsFormsApplication1\risposte.csv");
+
+
+            for (int i = 0; ; i++)
+            {
+                d = allLines[i].Split(';');
+                risposte risp = new risposte();
+                risp.setRisposte(Int32.Parse(d[0]) , d[1].ToString() , d[2].ToString() , d[3].ToString() );
+                risposte.Add(risp);
+            }
+        
+        }
+        public void caricaR(risposte r)
+        {
+            risposte.Add(r);
+        }
+
+        public void scriviFileRisposte()
+        {
+            var file = @"D:\GITHUB\OpenDay\PROGETTO\WindowsFormsApplication1\WindowsFormsApplication1\risposte.csv";
+            
+           
+            //stampa la riga
+                
+
+
+        }
+
+        public String getTipo(int indice)
+        {
+            return questionario.ElementAt(indice).getTipo();
+            
+        }
+
+        public String getTesto(int indice)
+        {
+            return questionario.ElementAt(indice).getTesto();
+
+        }
+        
+
     }
 }
--- CodiceCastelli/risposte.cs
+++ CodiceCastelli/risposte.cs
@@ -8,6 +8,40 @@ namespace WindowsFormsApplication1
 {
     class risposte
     {
+        int IdQuestionario;
+        
+        String Data;
+        String[] Domanda;
+        int numEl;
 
+       
+        public risposte()
+        {
+            int numEl = 0;
+            Data = DateTime.Today.ToShortDateString();
+        }
+
+        public void setRisposte(int id , String testo, String scelte, String risp)
+        {
+            this.IdQuestionario = id;
+            Domanda[numEl] = testo + scelte + risp;
+            numEl++;
+        }
+
+
+        public String getRow()
+        {
+            String row = IdQuestionario + Data;
+            for (int i = 0; i < numEl; i++)
+                row = row + Domanda[i];
+
+            return row;
+        }
+
+        public int getNumEl()
+        {
+            return numEl;
+        }
+    
     }
 }
